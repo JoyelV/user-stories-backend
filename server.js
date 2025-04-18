f347db4147216c45138ecc406858d2e4fbd128d0
Joyel <Varghesejoyel71@gmail.com>
2025-04-18 22:02:28 +0530

    server.js file updated

--- server.js
+++ server.js
@@ -6,7 +6,11 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'https://user-stories-event.vercel.app',
+  credentials: true, 
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected"))
